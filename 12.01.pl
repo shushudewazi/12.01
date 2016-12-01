4774c79a97a1c29b511cff401300eda68f9db1e4
linux <linux@linux-ElementaryOS.(none)>
2016-12-01 09:17:44 +0800

    xiaxiede

---
+++ 12.01.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+use strict;
+use diagnostics;
+use utf8;
+my $protein = 'protein sequence.pep';
+open my $PROTEIN ,'<',$protein or die "$0 : failed to input '$protein':$!\n";
+my @protein = <$PROTEIN>;
+print @protein, "\n";
+close $PROTEIN or warn "$0 : failed to input '$protein':$!\n";
+
+
+
