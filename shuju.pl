6a14c4dbdda9d5716b17ad2b5eb67542873eef3e
linux <linux@linux-ElementaryOS.(none)>
2016-12-01 09:55:15 +0800

    shuzu

---
+++ shuju.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+use diagnostics;
+use utf8;
+my @bases = ('A', 'C', 'G', 'T');
+my $bases = pop @bases;
+print  "here are the array element: ";
+print $bases,"\n";
+print "here are the array element: ";
+print @bases,"\n";
+
+
+
+
